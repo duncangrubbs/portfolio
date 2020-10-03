25a43f34059eb8096eb7e8819f94347edf6dce22
Duncan Grubbs <duncan.grubbs@gmail.com>
2020-10-03 12:51:41 -0400

    Blog edits.

--- src/routes/Blog.jsx
+++ src/routes/Blog.jsx
@@ -19,7 +19,12 @@ const Blog = () => (
        <Link to="/blog/oct012020">
         <WorkCard
           title="Modularize your API interface in Javascript"
-          text=""
+          text="After building a lot frontends in React, I got sick of re-writing the same code for data fetching.
+          I would have so many components making API calls, and in each one I would have a fetch
+          statement in which I would have to set headers, options, do JSON parsing, and error handling.
+          Avoiding duplicate code is a common programming best pratice, but it is too often ignored in the
+          web development space. Javascript has come a long way even in the last few years, and there are now
+          so many ways to modularize."
         />
       </Link>
     </div>
--- src/routes/blog/OCT012020.jsx
+++ src/routes/blog/OCT012020.jsx
@@ -18,9 +18,15 @@ const OCT012020 = () => (
     <div className="portfoliopage__content--article">
       <h1>Modularize your API interface in Javascript</h1>
       <p className="writing__para">
+        Before I begin, I would like to put a bit of a disclaimer that this is aimed at the
+        novice web developer. Many of the ideas I discuss are already widely used, and are
+        not my original idea. That being said, these are specific techniques that I was able
+        to actually use and improve my development process with.
+      </p>
+      <p className="writing__para">
         After building a lot frontends in React, I got sick of re-writing the same code for data fetching.
         I would have so many components making API calls, and in each one I would have a <code>fetch</code>
-        statement in which I would have to set headers, options, do JSON parsing, and error handling.
+        statement in which I would have to set headers and options, do JSON parsing, and error handling.
         Avoiding duplicate code is a common programming best pratice, but it is too often ignored in the
         web development space. Javascript has come a long way even in the last few years, and there are now
         so many ways to modularize.
@@ -35,11 +41,20 @@ const OCT012020 = () => (
         <li>JSON parsing</li>
       </ol>
       <p className="writing__para">
-        The solution I found is to create a few new files. This solution is not dependent on using React, as
+        The solution I found is to create a few new classes. This solution is not dependent on React, as
         these will be plain vanilla Javascript files. One will act as a <code>FetchService</code>. This
-        can be thought of the orchestrator, all requests will pass through this. Here you will declare your
-        <code>fetch</code> function that will override the default one, this way you can have headers hardcoded
-        to standardize all of the requests you are making. Of course if we need the option of changing them,
+        can be thought of the orchestrator, all requests will pass through this. The second class would be an
+        <code>ErrorService</code>. This will be used by the FetchService to handle errors if they arise.
+        The third class would be an <code>AuthService</code>. This would handle auth specific tasks such
+        as logging a user in, verifying they are logged in, and updating locally stored auth tokens.
+        Finally, you may want a file to store some widely used constants, like the base API url, the
+        version of the API you are using, an API key if applicable.
+      </p>
+      
+      <p className="writing__para">
+        In your <code>FetchService</code> you can declare your
+        <code>fetch</code> function that will override the default fetch API, this way you only have to declare your
+        headers once, to standardize all of the requests you are making. Of course if you need the option of changing them,
         you can offer an optional method parameter to set them. Then, you can either declare a specific
         function for each API endpoint you will be hitting, or just declare functions for <code>GET</code>,
         <code>POST</code>, <code>PUT</code>, and <code>DELETE</code> functions. These can simply take a
@@ -47,8 +62,13 @@ const OCT012020 = () => (
         if your API structure changes.
       </p>
       <p className="writing__para">
-        Another file would be a <code>ErrorService</code>. This will be used by the FetchService to handle
-        errors if they arise. In the <code>FetchService</code>, your overridden <code>fetch</code> method
+        In <code>AuthService</code>, you could declare public functions for logging a user in and out. These would
+        utilize the other services to make API requests, but they would validate and store the tokens themselves.
+        You could also provide functions for retrieving the auth token from storage and parsing it to return a
+        permission level, etc.
+      </p>
+      <p className="writing__para">
+        In the <code>FetchService</code>, your overridden <code>fetch</code> method
         should check the response code if it is bad, hand the response over to the <code>ErrorService</code>.
         From here, you can parse the error according to the REST API's format and even return an <code>html</code>
         component that nicely displays the error. In the component that made the original request, it would be
