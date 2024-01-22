653c638577953c2d171e8c31f06274a02b68f876
Duncan Grubbs <duncan.grubbs@gmail.com>
2024-01-22 12:14:18 -0500

    fix scroll tracker.

--- src/components/ScrollTracker.tsx
+++ src/components/ScrollTracker.tsx
@@ -1,30 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 const ScrollTracker = (): JSX.Element => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleScroll = (event: any) => {
+    const limit = Math.max(
+      document.body.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.clientHeight,
+      document.documentElement.scrollHeight,
+      document.documentElement.offsetHeight
+    );
+    // Do some math
+    const diff = limit - document.documentElement.clientHeight;
+    const diffPercent = diff / limit;
+    const percent =
+      event.target.scrollingElement.scrollTop / (limit * diffPercent);
+    const pixCalc = document.documentElement.clientWidth * percent;
+
+    // Update the width
+    if (ref.current) {
+      ref.current.style.width = `${pixCalc}px`;
+    }
+  };
+
   useEffect(() => {
-    const self = document.getElementById("scroll-tracker");
-
-    document.addEventListener("scroll", (evt: any) => {
-      const limit = Math.max(
-        document.body.scrollHeight,
-        document.body.offsetHeight,
-        document.documentElement.clientHeight,
-        document.documentElement.scrollHeight,
-        document.documentElement.offsetHeight
-      );
-      // Do some math
-      const diff = limit - document.documentElement.clientHeight;
-      const diffPercent = diff / limit;
-      const percent =
-        evt.target.scrollingElement.scrollTop / (limit * diffPercent);
-      const pixCalc = document.documentElement.clientWidth * percent;
-
-      // Update the width
-      self!.style.width = `${pixCalc}px`;
-    });
+    document.addEventListener("scroll", handleScroll);
+    return () => document.removeEventListener("scroll", handleScroll);
   }, []);
 
-  return <div className="fixed z-10 top-0 h-1 bg-red-600" />;
+  return <div ref={ref} className="fixed z-10 top-0 h-2 bg-emerald-600" />;
 };
 
 export default ScrollTracker;
